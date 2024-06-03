c3c0af574889ede4ce0037f86116abdd6862a442
umang70 <153925119+umang70@users.noreply.github.com>
2024-06-03 18:28:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Board from './TicTacToeGame/Board'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <Board />
+    </div>
+  )
+}
+
+export default App
---
+++ src/TicTacToeGame/Winner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Winner.css'
+
+function Winner() {
+  return (
+    <div class="neon">
+    <span class="text" data-text="thanks"> winner 🎖️ </span>
+    <span class="gradient"></span>
+    <span class="spotlight"></span>
+  </div>
+  )
+}
+
+export default Winner
